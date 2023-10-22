428a3f27f7998a3dd76670dfa604042a0cd427a3
xaviercl <xcl7992@hotmail.com>
2023-10-22 00:26:48 -0400

    Drew rectangle using default shader

--- Assets/Shapes/Shapes.cs
+++ Assets/Shapes/Shapes.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class Shapes
 {
+    private static Material material = new Material(Shader.Find("Unlit/PointShader"));
 
-    public static void Sphere(Vector3 position, float radius, Material material)
+    public static void Sphere(Vector3 position, float radius, Color color)
     {
         if (radius <= 0) return;
 
         var scale = new Vector3(radius * 2, radius * 2, 1);
         var meshPosition = Matrix4x4.TRS(position, Quaternion.identity, scale);
 
+        material.SetColor(1, color);
         var renderParams = new RenderParams(material);
         Graphics.RenderMesh(renderParams, DefaultMeshes.Quad, 0, meshPosition);
     }
--- Assets/Tiles.cs
+++ Assets/Tiles.cs
@@ -2,10 +2,9 @@ using UnityEngine;
 
 public class Tiles : MonoBehaviour
 {
-    public Material material;
 
     void Update()
     {
-        Shapes.Sphere(Vector3.zero, 1, material);
+        Shapes.Sphere(Vector3.zero, 1, Color.yellow);
     }
 }
