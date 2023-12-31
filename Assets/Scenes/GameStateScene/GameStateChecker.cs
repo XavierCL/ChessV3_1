acd46e5940ab849d04529a9e43c015e595707a98
xaviercl <xcl7992@hotmail.com>
2023-12-31 17:45:49 -0500

    Fixed v13

--- Assets/Scenes/GameStateScene/GameStateChecker.cs
+++ Assets/Scenes/GameStateScene/GameStateChecker.cs
@@ -105,11 +105,11 @@ public class GameStateChecker : MonoBehaviour
         return new List<GameStateInterface> {
             new V1GameStateFactory().FromFen("rnbqkb1r/pppp1p1p/5np1/4p3/2B1P3/2N2N2/PPPP1PPP/R1BQK2R b KQkq"),
             new V1GameStateFactory().FromFen("2kr1b2/1bp4r/p1nq1p2/3pp3/P3n1P1/3P4/1PP1QP1p/RNB1K1R1 w -"),
-            new V1GameStateFactory().FromFen("2kr1b2/1bp4r/p1nq1p2/3pp3/P3n1P1/3P4/1PP1QP2/RNB2KRr w -"),
             new V1GameStateFactory().FromFen("1r6/4b3/p6P/k7/2p5/8/5r2/7K b -"),
             new V1GameStateFactory().FromFen("4b2k/3p1p1p/3P1P1P/8/8/p1p1p3/P1P1P3/K2B4 w -"),
             new V1GameStateFactory().FromFen("rnbqk2r/pp2bpp1/5n1p/P2pp3/8/NP2PNPB/1pPPQP1P/R3K2R b KQkq"),
             new V1GameStateFactory().StartingPosition(),
+            new V1GameStateFactory().FromFen("2kr1b2/1bp4r/p1nq1p2/3pp3/P3n1P1/3P4/1PP1QP2/RNB2KRr w -"),
         };
     }
 }
